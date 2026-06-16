ca131bbd8101bd8f649e894d7e9f4f41e39e7768
Medha <48152485+medhanag29@users.noreply.github.com>
2026-06-16 12:45:41 +0530

    Update Firebase configuration and methods

--- src/firebase.js
+++ src/firebase.js
@@ -10,14 +10,23 @@ import "firebase/compat/storage";
 //   appId: "1:916425430497:web:ba178206cb6a0f12402ea0",
 //   measurementId: "G-XZSY6TT3GT",
 // };
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCqo0hZNEFtV8oojYVT_i__oiBi03q2BQg",
+//   authDomain: "rural-classroom-deb4b.firebaseapp.com",
+//   projectId: "rural-classroom-deb4b",
+//   storageBucket: "rural-classroom-deb4b.appspot.com",
+//   messagingSenderId: "564145504433",
+//   appId: "1:564145504433:web:73a9860f1332dd94a4a290",
+//   measurementId: "G-B18PGLY34Z",
+// };
 const firebaseConfig = {
-  apiKey: "AIzaSyCqo0hZNEFtV8oojYVT_i__oiBi03q2BQg",
-  authDomain: "rural-classroom-deb4b.firebaseapp.com",
-  projectId: "rural-classroom-deb4b",
-  storageBucket: "rural-classroom-deb4b.appspot.com",
-  messagingSenderId: "564145504433",
-  appId: "1:564145504433:web:73a9860f1332dd94a4a290",
-  measurementId: "G-B18PGLY34Z",
+  apiKey: "AIzaSyBVzQRzsjBzdJERv0TWEAHTgY0BDwMq4V4",
+  authDomain: "rural-classroom-2df1c.firebaseapp.com",
+  projectId: "rural-classroom-2df1c",
+  storageBucket: "rural-classroom-2df1c.firebasestorage.app",
+  messagingSenderId: "398275091037",
+  appId: "1:398275091037:web:1b4fb8351a7fd9fdfb4bb4",
+  measurementId: "G-GER8J59FT3"
 };
 firebase.initializeApp(firebaseConfig);
 export const auth = firebase.auth();
